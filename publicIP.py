1c22d2339e5569dcd4b6df4e0489409ca4ee7074
rbruder1980 <rob.bruder@gmail.com>
2018-07-23 06:17:11 -0400

    bug fix

--- publicIP.py
+++ publicIP.py
@@ -2,6 +2,6 @@ import urllib3
 
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://ip.42.pl/raw')
-publicIP = r.data
+publicIP = str(r.data)
 print("Your public IP address is: {}".format(publicIP))
 
